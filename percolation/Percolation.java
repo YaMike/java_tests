006297b1a62ea7f20ed7229666b1c64a21d1de17
michael <michael@T-LIHOLET.(none)>
2013-09-03 18:14:07 +0400

    Now it is time to debug...

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,20 +1,72 @@
 public class Percolation {
+	private enum SiteState {SS_EMPTY, SS_BLOCKED, SS_FILLED};
+	private SiteState[][] ssMatrix;
+	private int ssMatrixSize;
+
 	public Percolation(int N)              // create N-by-N grid, with all sites blocked
 	{
+		ssMatrixSize = N;
+		ssMatrix = new SiteState[N][N];
+		for (int i = 0; i < ssMatrix.length; ++i)
+			for (int j = 0; j < ssMatrix[i].length; ++j)
+				ssMatrix[i][j] = SiteState.SS_BLOCKED;
 	}
 	public void open(int i, int j)         // open site (row i, column j) if it is not already
 	{
+		ssMatrix[i][j] = SiteState.SS_EMPTY;
 	}
+
 	public boolean isOpen(int i, int j)    // is site (row i, column j) open?
 	{
-		return true;
+		return ssMatrix[i][j] == SiteState.SS_EMPTY;
 	}
+
 	public boolean isFull(int i, int j)    // is site (row i, column j) full?
 	{
-		return true;
+		return ssMatrix[i][j] == SiteState.SS_FILLED;
 	}
+
+	private int perlocate_to (int i, int j)
+	{
+		if (ssMatrix[i][j] == SiteState.SS_FILLED) {
+			return i;
+		}
+		/* left side */
+		if (j > 0 && ssMatrix[i][j-1] != SiteState.SS_BLOCKED) {
+			return perlocate_to (i, j-1);
+		}
+		/* rigth side */
+		if (j < (ssMatrix.length - 1) && ssMatrix[i][j+1] != SiteState.SS_BLOCKED) {
+			return perlocate_to (i, j+1);
+		}
+		/* bottom side */
+		if (i < (ssMatrixSize - 1) && ssMatrix[i+1][j] != SiteState.SS_BLOCKED) {
+			return perlocate_to (i + 1, j);
+		}
+		return i;
+	}
+
 	public boolean percolates()            // does the system percolate?
 	{
-		return true;
+		/* fill in first row empty sites */
+		for (int j = 0; j < ssMatrix.length; ++j) {
+			if (ssMatrix[0][j] == SiteState.SS_EMPTY) {
+				ssMatrix[0][j] = SiteState.SS_FILLED;
+			}
+		}
+		/* begin flood */
+		int level = 0, flood_level = 0;
+		for (int i_filled = 0; i_filled < ssMatrix.length; ++i_filled) {
+			if (ssMatrix[0][i_filled] == SiteState.SS_FILLED) {
+				if (flood_level < (level = perlocate_to (0, i_filled))) {
+					flood_level = level;
+				}
+			}
+		}
+		if (flood_level == (ssMatrixSize - 1)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,25 +1,99 @@
+import java.util.ArrayList;
+
 public class PercolationStats {
+	private double[] test_cases;
+	private double mean;
+	private double stddev;
+	static private int T;
+	static private int N;
+
+	private int empty_count = 0;
+
+	private class pair {
+		public int row;
+		public int col;
+		public pair (int r, int c) {
+			row = r;
+			col = c;
+		}
+		public pair (pair p) {
+			row = p.row;
+			col = p.col;
+		}
+	};
+	private class pairs {
+		private ArrayList<pair> p;
+		public pairs (int n) {
+			p = new ArrayList<pair>();
+			int row = 0, col = 0, count = n*n;
+			for (int i = 0; i < count; ++i) {
+				p.add(new pair(row,col));
+				col++;
+				if (col % n == 0) {
+					row++;
+					col = 0;
+				}
+			}
+		}
+		public pair take_one () {
+			int i_elem = StdRandom.uniform(0,p.size()-1);
+			pair lp = new pair(p.get(i_elem));
+			p.remove(i_elem);
+			return lp;
+		}
+		public int count () {
+			return p.size();
+		}
+	};
+
 	public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
 	{
+		pair lp;
+
+		int row, col;
+		Percolation p = new Percolation(N);
+		test_cases = new double[T];
+		for (int t = T; t < T; ++t) {
+			pairs ps = new pairs (N);
+			while (!p.percolates()) {
+				lp = ps.take_one ();
+				p.open(lp.row,lp.col);
+			}
+			test_cases[t] = ps.count();
+		}
 	}
+
 	public double mean()                     // sample mean of percolation threshold
 	{
-		return 0;
+		return mean = StdStats.mean(test_cases);
 	}
 	public double stddev()                   // sample standard deviation of percolation threshold
 	{
-		return 0;
+		return stddev = StdStats.stddev(test_cases);
 	}
 	public double confidenceLo()             // returns lower bound of the 95% confidence interval
 	{
-		return 0;
+		return (mean - 1.96*stddev/Math.sqrt(T));
 	}
 	public double confidenceHi()             // returns upper bound of the 95% confidence interval
 	{
-		return 0;
+		return (mean + 1.96*stddev/Math.sqrt(T));
 	}
 	public static void main(String[] args)   // test client, described below
 	{
-		int N = StdIn.readInt ();
+		N = StdIn.readInt ();
+		T = StdIn.readInt ();
+		if (N <= 0 || T <= 0) {
+			throw new java.lang.IllegalArgumentException(); 
+		}
+		/*
+		 *StdOut.printf ("Grid size: %d\n", N);
+		 *StdOut.printf ("Number of experiments: %d\n", T);
+		 */
+
+		PercolationStats PS = new PercolationStats (N, T);
+		StdOut.printf ("mean                    = %f\n", PS.mean());
+		StdOut.printf ("stddev                  = %f\n", PS.stddev());
+		StdOut.printf ("95%% confidence interval = %f, %f\n", PS.confidenceLo(), PS.confidenceHi());
 	}
 }
