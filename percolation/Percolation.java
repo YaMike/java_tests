4f088788444b1fd70333391a438fa0df00fd2c39
Michael <johnny-bgoode@mail.ru>
2013-09-03 13:46:06 +0400

    Now project can be built.

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,7 +1,20 @@
 public class Percolation {
-   public Percolation(int N)              // create N-by-N grid, with all sites blocked
-   public void open(int i, int j)         // open site (row i, column j) if it is not already
-   public boolean isOpen(int i, int j)    // is site (row i, column j) open?
-   public boolean isFull(int i, int j)    // is site (row i, column j) full?
-   public boolean percolates()            // does the system percolate?
+	public Percolation(int N)              // create N-by-N grid, with all sites blocked
+	{
+	}
+	public void open(int i, int j)         // open site (row i, column j) if it is not already
+	{
+	}
+	public boolean isOpen(int i, int j)    // is site (row i, column j) open?
+	{
+		return true;
+	}
+	public boolean isFull(int i, int j)    // is site (row i, column j) full?
+	{
+		return true;
+	}
+	public boolean percolates()            // does the system percolate?
+	{
+		return true;
+	}
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,8 +1,25 @@
 public class PercolationStats {
-   public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
-   public double mean()                     // sample mean of percolation threshold
-   public double stddev()                   // sample standard deviation of percolation threshold
-   public double confidenceLo()             // returns lower bound of the 95% confidence interval
-   public double confidenceHi()             // returns upper bound of the 95% confidence interval
-   public static void main(String[] args)   // test client, described below
+	public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
+	{
+	}
+	public double mean()                     // sample mean of percolation threshold
+	{
+		return 0;
+	}
+	public double stddev()                   // sample standard deviation of percolation threshold
+	{
+		return 0;
+	}
+	public double confidenceLo()             // returns lower bound of the 95% confidence interval
+	{
+		return 0;
+	}
+	public double confidenceHi()             // returns upper bound of the 95% confidence interval
+	{
+		return 0;
+	}
+	public static void main(String[] args)   // test client, described below
+	{
+		int N = StdIn.readInt ();
+	}
 }
