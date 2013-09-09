54e739e6156627fa6d97c4d416852e6e73dc55a2
michael <michael@T-LIHOLET.(none)>
2013-09-09 15:15:08 +0400

    Added testing files.

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,9 +1,7 @@
 public class Percolation {
-  private enum SiteState { SS_EMPTY, SS_BLOCKED, SS_FILLED };
+  private boolean[] opened;
   private int N;
   private WeightedQuickUnionUF qu;
-  private WeightedQuickUnionUF qu_open;
-  static private final boolean PercDebug = false;
 
   public Percolation(int size)              // create N-by-N grid, with all sites blocked
   {
@@ -13,41 +11,9 @@ public class Percolation {
     }
     N = size;
     qu = new WeightedQuickUnionUF(N*N+2);
-    qu_open = new WeightedQuickUnionUF(N*N+1);
-  }
-
-  private void infillState(int NewPos) {
-    int i = NewPos/N, j = NewPos % N;
-    if (   (NewPos < 0) 
-        || (NewPos > (N*N-1)) 
-        || !qu_open.connected(N*N, NewPos)
-        || qu.connected(N*N, NewPos)) {
-      return;
-    }
-    if (PercDebug) {
-      StdOut.printf("infill %d %d 32\n", i, j);
-      printOut();
-    }
-    qu.union(N*N, NewPos);
-    if (i == (N-1)) {
-      qu.union(N*N+1,NewPos);
-    }
-    /* check for neighbors states */
-    /* up */
-    if (i > 0) {
-      infillState (NewPos-N);
-    }
-    /* down */
-    if (i < (N-1)) {
-      infillState (NewPos+N);
-    }
-    /* left */
-    if (j > 0) {
-      infillState (NewPos-1);
-    }
-    /* right */
-    if (j < (N-1)) {
-      infillState (NewPos+1);
+    opened = new boolean[N*N+1];
+    for (int i = 0; i < N; i++) {
+      opened[i] = false;
     }
   }
 
@@ -58,43 +24,26 @@ public class Percolation {
       throw new java.lang.IndexOutOfBoundsException(str);
     }
     int CurPos = (i-1)*N+j-1;
-    boolean runFilling = false;
-    qu_open.union(N*N, CurPos);
-    if (PercDebug) {
-      StdOut.printf("Open %d %d 89\n", CurPos/N, CurPos % N);
-      printOut();
-    }
+    opened[CurPos] = true;
     if (i == 1) {
-      qu.union(N*N, CurPos);
-      runFilling = true;
-      if (PercDebug) {
-        StdOut.printf("set %d %d 97\n", CurPos/N, CurPos % N);
-        printOut();
-      }
+      qu.union(N*N,CurPos);
     }
-    int[] UpdPos = {-1, -1, -1, -1};
-    if (i > 1)     UpdPos[0] = CurPos-N;
-    if (i < (N)) UpdPos[1] = CurPos+N;
-    if (j > 1)     UpdPos[2] = CurPos-1;
-    if (j < (N)) UpdPos[3] = CurPos+1;
-    /* check if there any filled site around */
-    for (int k = 0; k < 4; ++k) {
-      if (UpdPos[k] > 0 && qu_open.connected(N*N, UpdPos[k]) && qu.connected(N*N, UpdPos[k])) {
-        qu.union(N*N, CurPos);
-        runFilling = true;
-        break;
-      }
+
+    if (i == N) {
+      qu.union(N*N+1,CurPos);
     }
-    /* recursively update site states if necessary */
-    if (runFilling) {
-      for (int k = 0; k < 4; ++k) {
-        if (UpdPos[k] > 0) {
-          infillState(UpdPos[k]);
-        }
-      }
+    
+    if (i > 1 && opened[CurPos-N]) {
+      qu.union(CurPos,CurPos-N);
     }
-    if (i == N && qu.connected(N*N, CurPos)) {
-      qu.union(N*N+1, CurPos);
+    if (i < (N) && opened[CurPos+N]) {
+      qu.union(CurPos,CurPos+N);
+    }
+    if (j > 1 && opened[CurPos-1]) {
+      qu.union(CurPos,CurPos-1);
+    }
+    if (j < (N) && opened[CurPos+1]) {
+      qu.union(CurPos,CurPos+1);
     }
   }
 
@@ -104,7 +53,7 @@ public class Percolation {
     if (i < 1 || j < 1 || i > N || j > N) {
       throw new java.lang.IndexOutOfBoundsException(str);
     }
-    return qu_open.connected(N*N,(i-1)*N+j-1);
+    return opened[(i-1)*N+j-1];
   }
 
   public boolean isFull(int i, int j)    // is site (row i, column j) full?
@@ -116,28 +65,9 @@ public class Percolation {
     return qu.connected(N*N, (i-1)*N+j-1);
   }
 
-  private void printOut() {
-    if (PercDebug) {
-    for (int i = 0; i < N; ++i) {
-      for (int j = 0; j < N; ++j) {
-        if (qu.connected(N*N,i*N+j)) {
-          StdOut.printf("O");
-        } else
-          if (qu_open.connected(N*N,i*N+j)) {
-            StdOut.printf(".");
-          } else {
-            StdOut.printf("#");
-          }
-      }
-      StdOut.printf("\n");
-    }
-    StdOut.printf("\n");
-    }
-  }
-
   public boolean percolates()            // does the system percolate?
   {
-    return qu.connected(N*N, N*N+1);
+    return qu.connected(N*N,N*N+1);
   }
 }
 
