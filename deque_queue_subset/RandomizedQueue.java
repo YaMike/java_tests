e6a368dd6df2f41d1b83e32fed586d67545a8aea
Michael <m.likholet@gmail.com>
2013-09-23 12:47:02 +0400

    Added test routine for RandomizedQueue

--- deque_queue_subset/RandomizedQueue.java
+++ deque_queue_subset/RandomizedQueue.java
@@ -25,6 +25,35 @@ class RandomizedQueue<Item> implements Iterable<Item> {
 
   public Iterator<Item> iterator()   // return an independent iterator over items in random order
   {
+    return new RandomIterator();
   }
 
+  public static void main(String[] args) {
+    RandomizedQueue<String> s = new RandomizedQueue<String>();
+    while (!StdIn.isEmpty()) {
+      String item = StdIn.readString();
+      if (item.length() < 1) break;
+      if (item.charAt(0) == '+') {
+        s.enqueue(item.substring(1));
+        StdOut.printf("Enqueue string: %s\n", item.substring(2));
+      } else
+      if (item.charAt(0) == '-') {
+        String removed_str = "";
+        removed_str = s.dequeue();
+        StdOut.printf("Dequeue item: %s\n", removed_str);
+      } else {
+        StdOut.printf("Finishing processing.\n");
+        break;
+      }
+      StdOut.printf("****************************\nRandomizedQueue (%d):\n", s.size());
+      for (String str: s) {
+        StdOut.printf("%s\n", str);
+      }
+      StdOut.printf("****************************\n");
+    }
+    StdOut.printf("Number of elems = %d :\n", s.size());
+    for (String str: s) {
+      StdOut.printf("%s\n", str);
+    }
+  }  
 }
