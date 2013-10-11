2b7a33539b220629980a1b2073f62c1e711f2c4d
michael <m.likholet@gmail.com>
2013-10-11 15:28:14 +0400

    94.58 %

--- collinear_brute_fast/Fast.java
+++ collinear_brute_fast/Fast.java
@@ -5,7 +5,7 @@ public class Fast {
 
   private static final int     MIN_COUNT = 3;
   private static final double  EPS = 1e-6;
-  private static final boolean DEBUG=true;
+  private static final boolean DEBUG=false;
 
   private static void processCollinearPoints(Point p, Point[] points, int start, int count) {
     Point[] parr = new Point[count+1];
@@ -14,14 +14,7 @@ public class Fast {
     System.arraycopy(points, start, parr, 1, count);
     Arrays.sort(parr);
     if (DEBUG) {
-      /*
-       *StdOut.printf("Origin point =%s, parr[0]=%s\n", p, parr[count]);
-       */
-      /*
-       *for (Point s: parr) {
-       *  StdOut.print(s+"->");
-       *}
-       */
+      StdOut.printf("Origin point =%s, parr[0]=%s, compare_res = %d\n", p, parr[count], parr[0].compareTo(p));
     }
     if (0 != parr[0].compareTo(p)) return;
     for (int i = 0; i < parr.length-1; i++) {
@@ -65,9 +58,9 @@ public class Fast {
       if (DEBUG) {
         StdOut.printf("i = %d\norigin point = %s\n", i, p);
       }
-      int count = origins.length-i-1;
+      int count = origins.length;
       Point[] points = new Point[count];
-      System.arraycopy(origins, i+1, points, 0, count);
+      System.arraycopy(origins, 0, points, 0, count);
       if (DEBUG) { printPointsArray("Origins copy", points); }
       Arrays.sort(points, p.SLOPE_ORDER);
       if (DEBUG) { printPointsArray("Sorted origins copy", points); }
@@ -80,7 +73,9 @@ public class Fast {
         pqSlope = p.slopeTo(q);
         if (j == 0) {
           prevSlope = pqSlope;
-          StdOut.printf("Slope=%f,%s,j=%d,col=%s,slopes cnt=%d\n", pqSlope, q, j, "unknown", slopesCount);
+          if (DEBUG) {
+            StdOut.printf("Slope=%f,%s,j=%d,col=%s,slopes cnt=%d\n", pqSlope, q, j, "unknown", slopesCount);
+          }
           continue;
         }
         if (   (Math.abs(pqSlope - prevSlope) < EPS)
@@ -100,7 +95,7 @@ public class Fast {
           if (DEBUG) {
             StdOut.printf("\n\\o/ Founded (%s,j = %d,start=%d, slopes count=%d)!\n", collinear ? "collinear" : "not collinear", j, start, slopesCount);
           }
-          processCollinearPoints(p, points, start, slopesCount-1);
+          processCollinearPoints(p, points, start, slopesCount);
           slopesCount = 0;
         }
         if (!collinear) {
