87e5d103e9d579e3625ec0e6a8b091a2742f84c6
YaMike <m.likholet@gmail.com>
2013-12-25 10:22:04 +0400

    Fixed test for le be stream.

--- le_be_stream/test.java
+++ le_be_stream/test.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.lang.Exception;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class test {
 	public static String IFileName = "input.txt";
@@ -13,18 +14,34 @@ public class test {
 
 	public class EndianDataInputStream extends DataInputStream {
 		EndianDataInputStream(InputStream is) {
-			super.DataInputStream(is);
+			super(is);
 		}
 	}
 
 	public static void main(String[] str) {
 		System.out.println("Test of new le/be stream");
+		DataInputStream dis;
+		DataOutputStream dos;
 		try {
-		DataInputStream dis = new DataInputStream(new FileInputStream(IFileName));
-		DataOutputStream dos = new DataOutputStream(new FileOutputStream(OFileName));
+			dis = new DataInputStream(new FileInputStream(IFileName));
+			dos = new DataOutputStream(new FileOutputStream(OFileName));
 		} catch (FileNotFoundException e) {
 			System.out.println("Files were not found! Terminating.");
 			return;
+		}
+		try {
+			dis.close();
+			dos.close();
+		} catch (IOException e) {
+			if (dis == null) {
+				System.err.println("Cannot close DataOutputStream!");
+			} else
+			if (dos == null) {
+				System.err.println("Some error occured while closing streams: " + e);
+				e.printStackTrace();
+			} else {
+				System.err.println("Cannot close DataInputStream!");
+			}
 		}
 	}
 }
