0bec63cc50efd6830701cb100b43213681bc6629
YaMike <m.likholet@gmail.com>
2014-02-16 15:01:14 +0400

    Fixed c++ variant of quick median.

--- quick_median/quickmedian.cpp
+++ quick_median/quickmedian.cpp
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  02/14/2014 04:44:03 PM
  *       Revision:  none
- *       Compiler:  gcc
+ *       Compiler:  g++
  *
  *         Author:  Michael Likholet <m.likholet@ya.ru>
  *        Company:  -
@@ -23,6 +23,7 @@
 #include <algorithm>
 #include <utility>
 #include <sstream>
+#include <chrono>
 
 #include <cstdlib>
 
@@ -64,26 +65,26 @@ int QuickMedian::Partition(int l, int r) {
   int t = data[r], i = l, j = l;
   for ( ; i < r; ++i) {
     if (data[i] < t) {
-      std::swap(&data[i], &data[j++]);
+      std::swap(data[i], data[j++]);
     }
   }
-  std::swap(&data[j], &data[r]);
+  std::swap(data[j], data[r]);
   return j;
 }
 
 int QuickMedian::randomizedPartition(int l, int r) {
   int pivot = std::rand() % (r - l + 1) + l;
-  swap(pivot, r);
+  std::swap(data[pivot], data[r]);
   return Partition(l,r);
 }
 
 namespace FisherYatsShuffle {
-  static void shuffle(vector<int> &input) {
-    std::vector<int>::iterator it = input.rbegin();
+  static void shuffle(std::vector<int> *input) {
+    std::vector<int>::reverse_iterator it = input->rbegin();
     int idx = 0;
-    for (it != input.rend(); it++) {
-      idx = std::rand() % (it - input.begin());
-      std::iter_swap(it,input.begin() + idx);
+    for ( ; it != input->rend(); it++) {
+      idx = std::rand() % (std::distance(it,input->rend()));
+      std::iter_swap(it,input->begin() + idx);
     }
   }
 }
@@ -91,43 +92,47 @@ namespace FisherYatsShuffle {
 int main(int argc, char *argv[]) {
 
   std::chrono::time_point<std::chrono::system_clock> start, end;
-  vector<int> *data = null;
+  std::vector<int> *data = NULL;
 
 	switch (argc) {
 		case 2:
-			std::cout << "Generating data..." << std::endl;
-      istringstream ss(argv[1]);
+    {
+      std::istringstream ss(argv[1]);
       int length;
       if (!(ss >> length)) {
         std::cerr << "Bad argument!" << std::endl;
         exit(1);
+      } else {
+        std::cout << "Generating " << length << " samples." << std::endl;
       }
       start = std::chrono::system_clock::now();
       data = new std::vector<int>(length);
-      for (std::vector<int>::iterator it = data.begin(); it != data.end(); ++it) {
-        *it = it - data.begin();
+      for (std::vector<int>::iterator it = data->begin(); it != data->end(); ++it) {
+        *it = it - data->begin();
       }
       FisherYatsShuffle::shuffle(data);
       end = std::chrono::system_clock::now();
       std::chrono::duration<double> elapsed_seconds = end - start; 
 			std::cout << "Generating took " << elapsed_seconds.count() << " secs" << std::endl;
 			break;
+    }
 		default:
       std::cerr << "Bad arguments count!" << std::endl;
 			exit(2);
 	};
-
-	std::cout << "Median = " << median << std::endl;
-	std::cout << "Time = " << time << " usecs" << std::endl;
+  if (data == NULL) {
+    std::cerr << "Wrong data. Terminating." << std::endl;
+    exit(3);
+  }
 
 	start = std::chrono::system_clock::now();
-
-  QuickMedian qm(data);
+  QuickMedian qm(*data);
   int median = qm.findMedian();
   end = std::chrono::system_clock::now();
   std::cout << "Median = " << median << std::endl;
   std::chrono::duration<double> elapsed_seconds = end - start; 
   std::cout << "Computation took " << elapsed_seconds.count() << " secs" << std::endl;
 
+  delete data;
 	return 0;
 }
