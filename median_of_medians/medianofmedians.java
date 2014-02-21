d6a4638866d6f7188161d4405e5e0a2c6f74ad7e
YaMike <m.likholet@gmail.com>
2014-02-21 12:45:00 +0400

    Added base file for java variant of median of medians.

---
+++ median_of_medians/medianofmedians.java
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  linearmedian.c
+ *
+ *    Description:  median of medians algorithm
+ *
+ *        Version:  1.0
+ *        Created:  02/21/2014 08:24:37 AM
+ *       Revision:  none
+ *       Compiler:	java
+ *
+ *         Author:	Michael Likholet <m.likholet@ya.ru>
+ *        Company:  -
+ *
+ * =====================================================================================
+ */
+
+import java.util.Arrays;
+import java.lang.Exception;
+import javatest.util.RandomData;
+
+class MedianOfMedians {
+	public boolean DEBUG=true;
+
+	public static void main(String[] args) {
+
+	}
+}
+
+class MedianSelection {
+	public MedianSelection() {
+	}
+
+}
+
+
