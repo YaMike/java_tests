0b9d86b9e15d5085a788d2fad0dd4dcc6b29fc77
YaMike <m.likholet@gmail.com>
2014-02-21 13:07:01 +0400

    Updated java version of median of medians.

--- median_of_medians/medianofmedians.java
+++ median_of_medians/medianofmedians.java
@@ -17,21 +17,49 @@
  */
 
 import java.util.Arrays;
+import java.util.Random;
 import java.lang.Exception;
 import javatest.util.RandomData;
 
 class MedianOfMedians {
-	public boolean DEBUG=true;
+	public static boolean DEBUG=true;
 
 	public static void main(String[] args) {
+		long start, stop;
+		start = System.nanoTime();
+		Integer[] data;
 
+		try {
+			System.out.println("Generating " + args[0] + " numbers");
+			data = RandomData.genData(Integer.parseInt(args[0]));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Cannot generate data for current parameters. Terminating.");
+			return;
+		}
+
+		stop = System.nanoTime();
+		System.out.println("Generating took " + ((stop-start)/1000) + " usecs");
+
+		MedianSelection<Integer> ms = new MedianSelection<Integer>(data);
 	}
 }
 
-class MedianSelection {
-	public MedianSelection() {
+class MedianSelection<T extends Comparable<? super T>> {
+	private static boolean DEBUG=true;
+	private T[] A;
+	private Random rand;
+
+	public MedianSelection(T[] A) {
+		this.rand = new Random();
+		this.A = A;
 	}
 
+	private void swap(int i, int j) {
+		T t = A[i];
+		A[i] = A[j];
+		A[j] = t;
+	}
 }
 
 
