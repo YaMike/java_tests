2c10f67b4aa25e7c4fab262043dd76a753843ac9
YaMike <m.likholet@gmail.com>
2014-05-16 12:33:19 +0400

    Init commit for 1003.

---
+++ timus/1002/main.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <iomanip>
+#include <inttypes.h>
+
+using namespace std;
+
+struct Test {
+	uint64_t number;
+	vector<string> words;
+
+	Test(uint64_t number) {
+		this->number = number;
+		int words_count;
+		cin >> words_count;
+		words.reserve(words_count);
+		string str;
+		for (int i = 0; i < words_count; ++i) {
+			cin >> str;
+			words.push_back(str);
+		}
+#ifdef DEBUG
+		cout << "test created: \"" << number << "\" ( ";
+		for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
+			cout << *it << " ";
+		}
+		cout << ")" << endl;
+#endif
+	}
+	void process() { 
+#ifdef DEBUG
+		cout << "processing " << number << endl; 
+#endif
+	}
+};
+
+int main(int argc, char *argv[]) {
+	vector<Test> tests;
+
+#ifdef DEBUG
+	cout << "reading data" << endl;
+#endif
+
+	while (!cin.eof()) {
+		int64_t number;
+		cin >> number;
+		if (number == -1) { break; }
+		tests.push_back(Test(number));
+	}
+
+	/* process data structures */
+	for (vector<Test>::iterator it = tests.begin(); it != tests.end(); ++it) {
+		it->process();
+	}
+
+	return 0;
+}
